f9581abc439f3581a81931f772a7a4739f19c96d
m0uZ <m.fraltsov@gmail.com>
2022-08-17 22:59:25 +0300

    Задачка про друзей и собаку

---
+++ Proga008_dogexp/Program.cs
@@ -0,0 +1,24 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+        int time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    }
+    if(friend == 2)
+    {
+        int time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    }
+        count += 1;
+}
+Console.WriteLine(count);
