b6894339f27f4873528225f12999ad022e1e7e69
m0uZ <m.fraltsov@gmail.com>
2022-08-17 21:13:03 +0300

    Добавил if else

---
+++ Proga005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "миша")
+{
+    Console.WriteLine("Здарова Мишаня!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
