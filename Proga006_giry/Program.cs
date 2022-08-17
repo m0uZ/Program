ec6620a1ebd0734a6777607be6ac1efcb00de7d6
m0uZ <m.fraltsov@gmail.com>
2022-08-17 21:29:31 +0300

    Добавил задачу найди макс

---
+++ Proga006_giry/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 6;
+int b = 3;
+int c = 8;
+int d = 1;
+int e = 9;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine(max);
