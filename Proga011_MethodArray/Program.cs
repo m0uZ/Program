be8acbcefa925215698e76c31cb58e6c26da38ba
m0uZ <m.fraltsov@gmail.com>
2022-08-20 21:44:19 +0300

    Нашли индекс элемента в массиве

---
+++ Proga011_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 21, 33, 16, 67, 56, 55, 34, 97, 56 };
+
+int n = array.Length;
+int find = 56;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
