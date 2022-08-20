0b8042265903ecef29ce728adc2aae7027af2f30
m0uZ <m.fraltsov@gmail.com>
2022-08-20 20:52:20 +0300

    Решение задачи про собаку

--- Proga008_dogexp/Program.cs
+++ Proga008_dogexp/Program.cs
@@ -5,20 +5,20 @@ int secondFriendSpeed = 2;
 int dogSpeed = 5;
 int friend = 2;
 
-while(distance > 10)
+while (distance > 10)
 {
-    if(friend == 1)
+    if (friend == 1)
     {
         int time = distance / (firstFriendSpeed + dogSpeed);
         friend = 2;
         distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
     }
-    if(friend == 2)
+    if (friend == 2)
     {
         int time = distance / (secondFriendSpeed + dogSpeed);
         friend = 1;
         distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
     }
-        count += 1;
+    count += 1;
 }
 Console.WriteLine(count);
